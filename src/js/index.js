ef4b047d64cb92e02dfac69ca334f6a2e9d63d56
slavik3333 <slavik3333@yahoo.com>
2019-07-16 12:50:02 +0300

    - width:ready; -pixel perfect: something so

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,42 @@
 /*$(document).ready(function() {
     $(".dropdown-toggle").dropdown();
 });*/
+$(document).ready(function(){
+    $('.slider').slick({
+        centerMode: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        speed: 500,
+    });
+    $('.albums-regular').slick({
+        infinite: true,
+        variableWidth: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        arrows: true,
+        prevArrow: $('.button-prev1'),
+        nextArrow: $('.button-next1'),
+    });
+    $('.albums-on-sale').slick({
+        infinite: true,
+        variableWidth: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        arrows: true,
+        prevArrow: $('.button-prev2'),
+        nextArrow: $('.button-next2'),
+    });
+});
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
